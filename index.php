75de669edc8cab70edfba9d3d28bcfc3aa7835da
“Maxwell <newton.maxwell@gmail.com>
2022-02-07 16:30:43 +0000

    started creating table

--- index.php
+++ index.php
@@ -11,7 +11,13 @@ $testinput = [
 <body>
 
 <main>
-    
+    <section class="new-item-form">
+        <form>
+            <h2>Add new item</h2>
+            <label for="itemname">Name: </label><input type="text" id="itemname" name="itemname">
+            <label for="type">Type: </label><input type="text" id="type" name="type">
+        </form>
+    </section>
     <?= bottlesHtml($bottles);?>
 
 </main>
